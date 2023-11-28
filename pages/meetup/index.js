f1c48a08c24e57b8238260110774b8766e2b4c39
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-11-28 11:54:18 -0800

    added axios post request on front-end

--- pages/meetup/index.js
+++ pages/meetup/index.js
@@ -1,10 +1,23 @@
 //our domain.com/meetup
 import NewMeetupForm from "@/components/meetups/NewMeetupForm"
+import { useRouter } from "next/router";
 
 const NewMeetUpPage = () => {
+    const router = useRouter()
 
     async function addMeetUpHandler(meetUpData) {
-        const response = await fetch('/api/new-meetup')
+        const response = await fetch('/api/new-meetup', {
+            method: 'POSt',
+            body: JSON.stringify(meetUpData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json();
+
+        console.log(data)
+
+        router.push('/')
     }
     return <NewMeetupForm onAddMeetup={addMeetUpHandler} />
 }
