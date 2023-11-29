038b08ee773a010180cb079477b4edc8e3538bad
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-11-29 11:18:52 -0800

    added GET call to project

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
-
+import { MongoClient } from "mongodb"
 import MeetupList from "../components/meetups/MeetupList"
 
-const Dummy_Meetups = [
-    {
-        id: 'm1',
-        title: 'First meetup',
-        image: 'https://picsum.photos/200',
-        address: 'Some Address 5, 12345 Some City',
-        description: 'This is our first meetUp'
-    },
-    {
-        id: 'm2',
-        title: 'Second meetup',
-        image: 'https://picsum.photos/200',
-        address: 'Some Address 5, 12345 Some City',
-        description: 'This is our first meetUp'
-    },
-    {
-        id: 'm3',
-        title: 'Third meetup',
-        image: 'https://picsum.photos/200',
-        address: 'Some Address 5, 12345 Some City',
-        description: 'This is our first meetUp'
-    }
-]
+// const Dummy_Meetups = [
+//     {
+//         id: 'm1',
+//         title: 'First meetup',
+//         image: 'https://picsum.photos/200',
+//         address: 'Some Address 5, 12345 Some City',
+//         description: 'This is our first meetUp'
+//     },
+//     {
+//         id: 'm2',
+//         title: 'Second meetup',
+//         image: 'https://picsum.photos/200',
+//         address: 'Some Address 5, 12345 Some City',
+//         description: 'This is our first meetUp'
+//     },
+//     {
+//         id: 'm3',
+//         title: 'Third meetup',
+//         image: 'https://picsum.photos/200',
+//         address: 'Some Address 5, 12345 Some City',
+//         description: 'This is our first meetUp'
+//     }
+// ]
 
 const HomePage = (props) => {
 
@@ -32,18 +32,54 @@ const HomePage = (props) => {
     )
 }
 
-export async function getStaticProps(){
-    //code written here is never accessed on the client side since it is only used in the build form 
-    //you can do things such as fetch data from an api 
+//the following ex is for API calls to the mongoDB
+
+export async function getStaticProps() {
+
+    const client = await MongoClient.connect(
+        'mongodb+srv://tyleraycock:rTs2dS6aWCj0WAiP@cluster0.afuimig.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db()
+
+    const meetupsCollection = db.collection('meetups') //can name this whatever you want 
+
+    const meetups = await meetupsCollection.find().toArray()
+
+    // console.log(meetups)
+
+    client.close()
+
     return {
         props: {
-            meetups: Dummy_Meetups
+            meetups: meetups.map(meetup => {
+                const { title, image, address } = meetup.data
+                return {
+                    id: meetup._id.toString(),
+                    title: title,
+                    image: image,
+                    address: address
+                }
+            })
         },
-        revalidate: 10
-        //revaildate will refresh the server side info every designated time you put in in seconds 
-        //ensuring the data is always up to date
+        revalidate: 1
     }
 }
+
+
+//this following example is for static dummy data
+
+// export async function getStaticProps(){
+//code written here is never accessed on the client side since it is only used in the build form 
+//you can do things such as fetch data from an api 
+// return {
+// props: {
+//     meetups: Dummy_Meetups
+// },
+// revalidate: 10
+//revaildate will refresh the server side info every designated time you put in in seconds 
+//ensuring the data is always up to date
+//     }
+// }
 
 // export async function getServerSideProps(context){
 //     //this will not run during the build process but ALWAYS on the server after deployment
